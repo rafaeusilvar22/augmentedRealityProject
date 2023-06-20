3f21e0762a6064a6914603f7ffbf6fa555c50408
rafaeusilvar22 <rafaelsilvar221@gmail.com>
2023-06-20 16:34:22 -0300

    add capybara low mobile

--- script.js
+++ script.js
@@ -72,3 +72,73 @@ btnNextAbout.addEventListener("click", () => {
   );
   btnNextAbout.setAttribute("visible", "false");
 });
+
+function loadPlaces(position) {
+  const params = {
+      radius: 300,    // search places not farther than this value (in meters)
+      clientId: 'N3WJ41R5UHA0FPIN4FWGK5ZJXUEAUJEOZ1BOI4SGJCZEL0KK',
+      clientSecret: 'CEMLIZYHN41GTFZG5LUYUWI2TNFQFHHJJY3QD00YFD43MAWZ',
+      version: '20300101',    // foursquare versioning, required but unuseful for this demo
+  };
+
+  // CORS Proxy to avoid CORS problems
+  const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+
+  // Foursquare API (limit param: number of maximum places to fetch)
+  const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
+      &ll=${position.latitude},${position.longitude}
+      &radius=${params.radius}
+      &client_id=${params.clientId}
+      &client_secret=${params.clientSecret}
+      &limit=30 
+      &v=${params.version}`;
+  return fetch(endpoint)
+      .then((res) => {
+          return res.json()
+              .then((resp) => {
+                  return resp.response.venues;
+              })
+      })
+      .catch((err) => {
+          console.error('Error with places API', err);
+      })
+};
+
+
+window.onload = () => {
+  const scene = document.querySelector('a-scene');
+
+  // first get current user location
+  return navigator.geolocation.getCurrentPosition(function (position) {
+
+      // than use it to load from remote APIs some places nearby
+      loadPlaces(position.coords)
+          .then((places) => {
+              places.forEach((place) => {
+                  const latitude = place.location.lat;
+                  const longitude = place.location.lng;
+
+                  // add place name
+                  const placeText = document.createElement('a-link');
+                  placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                  placeText.setAttribute('title', place.name);
+                  placeText.setAttribute('scale', '15 15 15');
+                  
+                  placeText.addEventListener('loaded', () => {
+                      window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+                  });
+
+                  scene.appendChild(placeText);
+
+                  console.log("Aquiiiiiiii ",placeText)
+              });
+          })
+  },
+      (err) => console.error('Error in retrieving position', err),
+      {
+          enableHighAccuracy: true,
+          maximumAge: 0,
+          timeout: 27000,
+      }
+  );
+};
